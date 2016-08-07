319ee062c8cf0586ddad8683cf3cc1d0649ca5cb
Adam Sears <apsears@users.noreply.github.com>
2016-08-07 08:10:47 -0400

    Update Export_html_glossary.py

--- Export_html_glossary.py
+++ Export_html_glossary.py
@@ -1,31 +1,20 @@
-# export_html_glossary Anki add-on
-# based on export_to_html Anki add-on
-# Author Peter Borkuti
-
-# Exports a desk to a two-column html file. The first column is the answer, the second
-# column is the question.
-# It is good for creating a glossary based on an Anki deck.
-
-# The output html file must be placed in the Anki's user media folder.
-# If the style must be changed, create a custom.css file in the media folder.
-
+from anki.hooks import wrap
+from anki.sync import Syncer
 from anki.lang import _
-from anki.hooks import addHook
 from anki.exporting import TextCardExporter
-from aqt.utils import getText
-from aqt.utils import showInfo
-from anki.cards import Card
-from anki.utils import  ids2str
+
 import re
-import sys,os
-import time
+import os
+
+TARGETFOLDER = './s3'
+# S3BUCKET = os.environ['S3BUCKET']
+NCARDS = 20
 
 class MyTextCardExporter(TextCardExporter):
-
     key = _("Export deck as Html Glossary")
     ext = ".htm"
     hideTags = True
-    htmlBefore="""
+    htmlBefore = """
 <!DOCTYPE html PUBLIC "-/W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -33,15 +22,16 @@ class MyTextCardExporter(TextCardExporter):
 <title>Untitled Document</title>
 <style type="text/css">
 
+
 .Question{
-    float: right;
+    float: left;
     height: auto;
-    width: 40%;    
+    width: 30%;
 }
 .Answer{
     float: right;
     height: auto;
-    width: 40%;    
+    width: 30%;
 }
 
 .Answer,.Question{
@@ -56,7 +46,7 @@ class MyTextCardExporter(TextCardExporter):
     text-align: center;
     clear:both;
     height: auto;
-    width: 100%;
+    width: 640px;
     overflow: visible;
     border-top-width: thin;
     border-right-width: thin;
@@ -70,11 +60,11 @@ class MyTextCardExporter(TextCardExporter):
 }
 
 .Card:after {
-    content: '.'; 
-    display: block; 
-    clear: both; 
-    visibility: hidden; 
-    height: 0; 
+    content: '.';
+    display: block;
+    clear: both;
+    visibility: hidden;
+    height: 0;
     line-height: 0;
 }
 img {
@@ -87,18 +77,20 @@ img {
     left:0;
 }
 
+
+
 </style>
 <link type="text/css" rel="stylesheet" href="custom.css">
 </head>
-<body>
+<body style="font-size:200%">
     """
-    htmlAfter="""
+    htmlAfter = """
     </body>
 </html>
     """
+
     def __init__(self, col):
         TextCardExporter.__init__(self, col)
-        
 
     def escapeText(self, text):
         "Escape newlines, tabs and CSS."
@@ -106,30 +98,50 @@ img {
         text = text.replace("\t", "")
         text = re.sub("(?i)<style>.*?</style>", "", text)
         return text
- 
-    
+
     def doExport(self, file):
-        ids = self.cardIds()
+        cards = self.cardIds()
 
         def esc(s):
             # strip off the repeated question in answer if exists
             s = re.sub("(?si)^.*<hr id=answer>\n*", "", s)
-            s=  re.sub("(?si)<style.*?>.*?</style>", "", s)
+            s = re.sub("(?si)<style.*?>.*?</style>", "", s)
             return self.escapeText(s)
+
         out = ""
 
-        for cid in ids:
-            c = self.col.getCard(cid)
-            out += "<div class=Card>"
-            out += "  <div class=Question>" + esc(c.q()) + "</div>"
-            out += "  <div class=Answer>" + esc(c.a()) + "</div>"
-            out += "</div>" 
-        out= self.htmlBefore + out + self.htmlAfter
+        for c in cards:
+            if c:
+            # c = self.col.getCard(cid)
+                out += "<div class=Card>"
+                out += "  <div class=Question>" + esc(c.q()) + "</div>"
+                out += "  <div class=Answer>" + esc(c.a()) + "</div>"
+                out += "</div>"
+        out = self.htmlBefore + out + self.htmlAfter
         file.write(out.encode("utf-8"))
 
-def addMyExporter(exps):
-    def theid(obj):
-        return ("%s (*%s)" % (obj.key, obj.ext), obj)
-    exps.append(theid(MyTextCardExporter))
-    
-addHook("exportersList", addMyExporter)
+    def cardIds(self):
+        cardList = [self.col.sched.getCard() for _ in range(NCARDS)]
+        cardList = filter(lambda x: x, cardList)
+
+        for _ in range(NCARDS - len(cardList)):
+            cardList.append(self.col.sched._getLrnCard())
+        cardList = filter(lambda x: x, cardList)
+
+        for _ in range(NCARDS - len(cardList)):
+            cardList.append(self.col.sched._getRevCard())
+        cardList = filter(lambda x: x, cardList)
+
+
+        return sorted(cardList, key=lambda x: x.template(), reverse=False)
+
+def exportScheduled(self):
+    exp = MyTextCardExporter(self.col)
+    path = os.path.join(TARGETFOLDER,'scheduledVocab.html')
+    file = open(path, "wb")
+    exp.doExport(file)
+    # os.system("aws s3 sync --acl public-read %s s3://%s" % (TARGETFOLDER, S3BUCKET))
+    return "success"
+
+
+Syncer.sync = wrap(Syncer.sync, exportScheduled)
